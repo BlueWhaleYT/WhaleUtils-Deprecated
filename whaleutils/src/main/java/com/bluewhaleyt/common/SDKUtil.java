d07e5dadc123735a2445430bee55650beb398cdc
bluewhaleyt <cocohau155@gmail.com>
2023-02-10 15:16:39 +0800

    complete SDKUtil

--- whaleutils/src/main/java/com/bluewhaleyt/common/SDKUtil.java
+++ whaleutils/src/main/java/com/bluewhaleyt/common/SDKUtil.java
@@ -6,9 +6,29 @@ import androidx.annotation.ChecksSdkIntAtLeast;
 
 public class SDKUtil {
 
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.LOLLIPOP)
+    public static boolean isAtLeastSDK21() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+    public static boolean isAtLeastSDK22() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1;
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.M)
+    public static boolean isAtLeastSDK23() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N)
     public static boolean isAtLeastSDK24() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N_MR1)
+    public static boolean isAtLeastSDK25() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1;
     }
 
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O)
@@ -16,9 +36,24 @@ public class SDKUtil {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
     }
 
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O_MR1)
+    public static boolean isAtLeastSDK27() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1;
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.P)
+    public static boolean isAtLeastSDK28() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.P;
+    }
+
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.Q)
     public static boolean isAtLeastSDK29() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q;
+    }
+
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
+    public static boolean isAtLeastSDK30() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R;
     }
 
     @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)

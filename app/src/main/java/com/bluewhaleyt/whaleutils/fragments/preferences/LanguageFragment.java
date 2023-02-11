f518b04f5e60c03c5ede79e650a1c38860f92cd0
bluewhaleyt <cocohau155@gmail.com>
2023-02-11 17:20:15 +0800

    update sample app

--- app/src/main/java/com/bluewhaleyt/whaleutils/fragments/preferences/LanguageFragment.java
+++ app/src/main/java/com/bluewhaleyt/whaleutils/fragments/preferences/LanguageFragment.java
@@ -2,7 +2,9 @@ package com.bluewhaleyt.whaleutils.fragments.preferences;
 
 import android.os.Bundle;
 
+import com.bluewhaleyt.common.DynamicColorsUtil;
 import com.bluewhaleyt.component.preferences.CustomPreferenceFragment;
+import com.bluewhaleyt.component.preferences.material3.Material3ButtonPreference;
 import com.bluewhaleyt.component.snackbar.SnackbarUtil;
 import com.bluewhaleyt.whaleutils.R;
 
@@ -15,6 +17,15 @@ public class LanguageFragment extends CustomPreferenceFragment {
 
     private void init() {
         try {
+
+            DynamicColorsUtil dynamicColors = new DynamicColorsUtil(requireActivity());
+
+            var componentBtnTest = findPreference("component_btn_test");
+
+            componentBtnTest.setOnPreferenceClickListener(preference -> {
+                SnackbarUtil.makeSnackbar(requireActivity(), "test 123");
+                return true;
+            });
 
         } catch (Exception e) {
             SnackbarUtil.makeErrorSnackbar(getActivity(), e.getMessage(), e.toString());
--- whaleutils/src/main/java/com/bluewhaleyt/component/preferences/material3/Material3ButtonPreference.java
+++ whaleutils/src/main/java/com/bluewhaleyt/component/preferences/material3/Material3ButtonPreference.java
@@ -1,15 +1,21 @@
 package com.bluewhaleyt.component.preferences.material3;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.preference.PreferenceViewHolder;
 
 import com.bluewhaleyt.component.preferences.ButtonPreference;
 import com.bluewhaleyt.whaleutils.R;
 
 public class Material3ButtonPreference extends ButtonPreference {
+
+    private CardView cardView;
+
     public Material3ButtonPreference(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         setView();
